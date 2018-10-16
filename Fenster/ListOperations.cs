2bbd9b6d9357cf07a6567dbd52cc525f5c420b66
Samuel Giesser <Samuel.Giesser@copitos.de>
2018-10-16 14:16:37 +0200

    update

---
+++ Fenster/ListOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fenster
+{   
+    //Diese Klasse verfügt über Funktionen für das In Array
+    public class ListOperations : List<Rectangle>
+    {
+         
+    }
+}
